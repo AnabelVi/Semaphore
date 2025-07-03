616c716bb9757283a29c9bd7d0f5d4cda0dbfbbe
Anabel Villarreal <anabelsvillarreal85@gmail.com>
2025-07-03 11:06:46 -0300

    Se agrega el file main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+const int button = PIN5;
+
+void setup()
+{
+    pinMode(red, OUTPUT);
+    pinMode(yellow, OUTPUT);
+    pinMode(green, OUTPUT);
+    pinMode(button, OUTPUT);
+
+    digitalWrite(red, LOW);
+    digitalWrite(yellow, LOW);
+    digitalWrite(green, LOW);
+
+}
